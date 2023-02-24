a5582aad5aa5075cc76b76814bbaac8ae47df400
Prashant19987 <78965518+Prashant19987@users.noreply.github.com>
2023-02-25 00:41:48 +0530

    Add files via upload
    First Commit

---
+++ github.java
@@ -0,0 +1,14 @@
+package git;
+
+public class github {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	    System.out.println("Hi");
+	    System.out.println("Prashant Kumar");
+	    System.out.println("Github");
+	    System.out.println("Java Web Development");
+
+	}
+
+}
